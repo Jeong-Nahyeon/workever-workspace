55185ec7f60054c453b87a78ee364c96a8ac3857
Yun-Nakyung <93471500+Yun-Nakyung@users.noreply.github.com>
2022-02-09 16:49:46 +0900

    [나경] docs : user 클래스 추가
    docs : user 클래스 추가
    author : 윤나경
    
    User 정보를 담을 수 있는 Vo클래스 추가
    
    - add User.java

--- workever/src/main/java/com/workever/wk/user/model/vo/User.java
+++ workever/src/main/java/com/workever/wk/user/model/vo/User.java
@@ -1,6 +1,31 @@
 package com.workever.wk.user.model.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter @Getter
+@ToString
 public class User {
 
-	private String hi;
+	private String userNo;
+	private String deptNo;
+	private String comNo;
+	private String userEmail;
+	private String userName;
+	private String userPhone;
+	private String userPwd;
+	private String userRank;
+	private String userJoinDate;
+	private String userResDate;
+	private String userStatus;
+	private String userEnabled;
+	private String userFilePath;
+	private String userAuth;
+	private String userAnnualDate;
+	private String userUseDate;
 }
