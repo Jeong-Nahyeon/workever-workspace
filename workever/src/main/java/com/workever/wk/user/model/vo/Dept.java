8ac38744152517150b9989986b96e2ff0427bdd0
Yun-Nakyung <93471500+Yun-Nakyung@users.noreply.github.com>
2022-02-15 22:38:58 +0900

    [나경] docs : 부서VO 클래스 추가
    docs : 부서Vo 추가
    author : 윤나경
    
    부서정보를 담기 위한 VO클래스 추가
    
    - add workever/wk/user/model/vo/Dept.java

--- workever/src/main/java/com/workever/wk/user/model/vo/Dept.java
+++ workever/src/main/java/com/workever/wk/user/model/vo/Dept.java
@@ -1,5 +1,18 @@
 package com.workever.wk.user.model.vo;
 
-public class Dept {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter @Getter
+@ToString
+public class Dept {
+	
+	private String deptNo;
+	private String comNo;
+	private String deptName;
 }
